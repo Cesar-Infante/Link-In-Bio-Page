427eece5f29e38dae29c831248f423072fbcc490
Cesar's PC <cesar261110@gmaill.com>
2022-08-16 13:05:20 -0700

    Testing vanta.js

---
+++ index.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', () => {
+    VANTA.BIRDS({
+        el: "#vanta",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00
+    })
+})
+
+
