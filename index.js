b41acb9e7a5afa6a909c2f7b347eecb0495f1fda
Cesar's PC <cesar261110@gmaill.com>
2022-08-16 13:43:40 -0700

    vanta.js implementation

--- index.js
+++ index.js
@@ -7,8 +7,22 @@ window.addEventListener('DOMContentLoaded', () => {
         minHeight: 200.00,
         minWidth: 200.00,
         scale: 1.00,
-        scaleMobile: 1.00
+        scaleMobile: 1.00,
+        birdSize: 1.10,
+        wingSpan: 27.00,
+        speedLimit: 4.00,
+        separation: 28.00,
+        alignment: 23.00,
+        cohesion: 21.00,
+        quantity: 3.00,
+        backgroundAlpha: 0.0
     })
+    
+    setTimeout(() => {
+        const main = document.querySelector('main')
+        main.style.opacity = 1
+    }, 1000)
 })
+
 
 
